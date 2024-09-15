f3771febd7e3d0a934cbf305fbb8a1f3cbd1cdad
SAMIULLAH2105 <samiullah21january@gmail.com>
2024-09-15 21:54:36 +0500

    added ../ before src

--- src/components/ProductData.js
+++ src/components/ProductData.js
@@ -17,43 +17,43 @@ export const products = [
   { 
     id: 3, 
     name: 'HOWO 6X4 DUMP TRUCK MIDDLE LIFTING RHD', 
-    image1: 'src/assets/products/maronTruckRight.jpg', 
-    image2: 'src/assets/products/maronTruckFront.jpg',
+    image1: '../src/assets/products/maronTruckRight.jpg', 
+    image2: '../src/assets/products/maronTruckFront.jpg',
     description: 'This right-hand drive dump truck combines comfort and functionality, perfect for diverse terrains and loading conditions.'
   },
   { 
     id: 4, 
     name: 'HOWO 8X4 CARGO TRUCK', 
-    image1: 'src/assets/products/blueTruckLeftonly.jpg', 
-    image2: 'src/assets/products/blackTruckRight.jpg',
+    image1: '../src/assets/products/blueTruckLeftonly.jpg', 
+    image2: '../src/assets/products/blackTruckRight.jpg',
     description: 'Ideal for transporting large payloads, this 8x4 cargo truck offers unmatched reliability and a spacious cargo area.'
   },
   { 
     id: 5, 
     name: 'HOWO 8X4 CHASSIS TRUCK LEFT', 
-    image1: 'src/assets/products/redTruckLeft.jpg', 
-    image2: 'src/assets/products/redTruckRight.jpg',
+    image1: '../src/assets/products/redTruckLeft.jpg', 
+    image2: '../src/assets/products/redTruckRight.jpg',
     description: 'This chassis truck features a left-hand drive setup, ensuring safety and comfort in logistics operations.'
   },
   { 
     id: 6, 
     name: 'HOWO 8X4 CHASSIS TRUCK RHD', 
-    image1: 'src/assets/products/greenTruckRight.jpg', 
-    image2: 'src/assets/products/greenTruckInterior.jpg',
+    image1: '../src/assets/products/greenTruckRight.jpg', 
+    image2: '../src/assets/products/greenTruckInterior.jpg',
     description: 'A right-hand drive chassis truck that provides a robust platform for various modifications and applications.'
   },
   { 
     id: 7, 
     name: 'HOWO 8X4 DUMP TRUCK', 
-    image1: 'src/assets/products/blueTruckLeftonly.jpg', 
-    image2: 'src/assets/products/blueTruckLeftonly.jpg',
+    image1: '../src/assets/products/blueTruckLeftonly.jpg', 
+    image2: '../src/assets/products/blueTruckLeftonly.jpg',
     description: 'This heavy-duty 8x4 dump truck is engineered to perform in the most demanding of conditions, offering exceptional durability.'
   },
   { 
     id: 8, 
     name: 'HOWO 8X4 DUMP TRUCK GREEN', 
-    image1: 'src/assets/products/bigredTruck.jpg', 
-    image2: 'src/assets/products/bigredTruck.jpg',
+    image1: '../src/assets/products/bigredTruck.jpg', 
+    image2: '../src/assets/products/bigredTruck.jpg',
     description: 'Eco-friendly and powerful, this green dump truck combines sustainability with high performance for all your construction needs.'
   }
 ];
